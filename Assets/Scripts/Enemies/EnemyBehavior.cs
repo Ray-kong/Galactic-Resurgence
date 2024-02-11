cf2fb7ddcf0130015159f0d65ae5ab91d1cd592d
ThomasBelk <122413223+ThomasBelk@users.noreply.github.com>
2024-02-11 13:58:38 -0500

    Enemy AI
    Enemy patrol till the player is in range and they then got try and shoot him. The shooting isn't done yet cause I wanted to make sure that it was consistent with how our player is going to shoot.

---
+++ Assets/Scripts/Enemies/EnemyBehavior.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    #region SerializableVars
+    [SerializeField] private float health = 100f;
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private LayerMask groundMask, playerMask;
+
+    [SerializeField] private Vector3 walkPoint;
+    [SerializeField] private float walkPointRange;
+    [SerializeField] private bool walkPointIsSet = false;
+
+    [SerializeField] private float timeBetweenAttacks; // may not need later if we have some sort of gun class
+    [SerializeField] private bool hasAttacked;
+
+    [SerializeField] private float sightRange, attackRange;
+    [SerializeField] private bool playerInSight, playerInAttackRange;
+    #endregion
+
+    private Transform player;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        playerInSight = Physics.CheckSphere(transform.position, sightRange, playerMask);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, playerMask);
+        
+        if (!playerInSight && !playerInAttackRange)
+        {
+            Patrolling();
+        } 
+        else if (playerInSight && !playerInAttackRange)
+        {
+            ChasePlayer();
+        }
+        else
+        {
+            AttackPlayer();
+        }
+    }
+
+    private void Patrolling()
+    {
+        if (!walkPointIsSet)
+        {
+            SearchWalkPoint();
+        }
+        else
+        {
+            agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if (distanceToWalkPoint.magnitude < 1f)
+        {
+            walkPointIsSet = false;
+        }
+    }
+
+    private void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, groundMask))
+        {
+            walkPointIsSet = true;
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+        transform.LookAt(player);
+
+        // attack logic here
+        if (!hasAttacked)
+        {
+            Debug.Log("Attacking Player");
+            hasAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        hasAttacked = false;
+    }
+
+    // visual indicator
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+}
