49b6384a77958efc6e80bf9e41ae880259175f03
RayKong <ray.hyun.kong@gmail.com>
2024-02-18 01:52:14 -0500

    add free asset

--- Assets/Prefabs/Temp.cs
+++
@@ -1,7 +0,0 @@
-
-
-public class Temp
-{
-    //I know this is prefab folder, but I'm need something inside this to so github can track this folder :)
-    //delete this file once you add prefab to it
-}
