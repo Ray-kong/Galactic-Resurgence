7bcd9ff5f9d78426f816861bb8d2749f10c29b65
ThomasBelk <122413223+ThomasBelk@users.noreply.github.com>
2024-03-28 18:38:57 -0400

    We'll see

---
+++ Assets/Scripts/Enemies/EnemyAI.cs
@@ -0,0 +1,155 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    private enum EnemyState
+    {
+        PATROL,
+        ATTACK,
+        CHASE,
+        DEAD
+    }
+
+    private EnemyState currentState;
+    public NavMeshAgent agent;
+    public Transform player;
+    public float enemySpeed = 3.5f;
+    public float chaseDistance = 20f;
+    public float attackDistance = 15f;
+
+    private float distanceToPlayer;
+    
+    public GameObject[] wanderPoints;
+    private int currentDestIndex = 0;
+
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = EnemyState.PATROL;
+        if (agent == null)
+        {
+            agent = GetComponent<NavMeshAgent>();
+        }
+
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+        wanderPoints = GameObject.FindGameObjectsWithTag("WanderPoint");
+        anim = GetComponent<Animator>();
+        agent.speed = enemySpeed;
+        agent.stoppingDistance = attackDistance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        switch (currentState)
+        {
+            case EnemyState.PATROL:
+                UpdatePatrolState();
+                break;
+            case EnemyState.CHASE:
+                UpdateChaseState();
+                break;
+            case EnemyState.ATTACK:
+                UpdateAttackState();
+                break;
+            case EnemyState.DEAD:
+                UpdateDeadState();
+                break;
+        }
+        SetBlendSpaceFloats();
+    }
+
+    private void UpdatePatrolState()
+    {
+        agent.stoppingDistance = 0f;
+        agent.SetDestination(wanderPoints[currentDestIndex].transform.position);
+        PatrolPoints();
+        if (distanceToPlayer <= chaseDistance)
+        {
+            currentState = EnemyState.CHASE;
+        }
+        //agent.SetDestination(player.position);
+    }
+
+    private void UpdateChaseState()
+    {
+        agent.stoppingDistance = attackDistance;
+        if (distanceToPlayer <= attackDistance)
+        {
+            currentState = EnemyState.ATTACK;
+        } else if (distanceToPlayer > chaseDistance)
+        {
+            currentState = EnemyState.PATROL;
+        }
+        agent.SetDestination(player.position);
+    }
+    
+    private void UpdateAttackState()
+    {
+        agent.stoppingDistance = attackDistance;
+        FaceTarget(player.position);
+        if (distanceToPlayer > attackDistance)
+        {
+            currentState = EnemyState.CHASE;
+        }
+    }
+
+    private void UpdateDeadState()
+    {
+        agent.speed = 0f;
+    }
+
+    private void SetBlendSpaceFloats()
+    {
+        Vector3 velocity = agent.velocity;
+
+        Vector3 forward = transform.forward;
+        Vector3 right = transform.right;
+
+        float forwardSpeed = Vector3.Dot(velocity, forward);
+        float sidewaysSpeed = Vector3.Dot(velocity, right);
+
+        float x = Mathf.Clamp(sidewaysSpeed/ enemySpeed, -1f, 1f);
+        float y = Mathf.Clamp(forwardSpeed/ enemySpeed, -1f, 1f);
+        
+        anim.SetFloat("x", x);
+        anim.SetFloat("y", y);
+
+        // Debug.Log("Forward Speed: " + forwardSpeed);
+        // Debug.Log("Sideways Speed: " + sidewaysSpeed);
+    }
+    
+    private void PatrolPoints()
+    {
+        if (Vector3.Distance(transform.position, wanderPoints[currentDestIndex].transform.position) < 1)
+        {
+            currentDestIndex = (currentDestIndex + 1) % wanderPoints.Length;
+            Vector3 nextDestination = wanderPoints[currentDestIndex].transform.position;
+            agent.SetDestination(nextDestination);
+        }
+    }
+    
+    private void FaceTarget(Vector3 target) 
+    {
+        Vector3 direction = (target - transform.position).normalized;
+        direction.y = 0;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, 10 * Time.deltaTime);
+    }
+    
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackDistance);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, chaseDistance);
+    }
+}
